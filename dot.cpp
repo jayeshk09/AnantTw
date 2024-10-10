164d911276d8a9224ccfcbca2a2f94c55420cf45
Anant Goyal <2023ucp1679@mnit.ac.in>
2024-10-10 12:52:51 +0530

    dot.cpp

---
+++ dot.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+	vector<int> v1 = {1,2,3};
+	vector<int> v2 = {4,5,6};
+	vector<int> v3;
+	for (int i=0;i<v1.size();i++)
+	{
+		v3[i] = v1[i]*v2[i];
+	}
+}
